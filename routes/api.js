bee4a932ffcaf184a1b6cf474be06d77a21745ac
basnetrajpradip <basnetrajpradip@gmail.com>
2024-03-24 00:29:55 +0545

    Configure route auth

--- routes/api.js
+++ routes/api.js
@@ -14,15 +14,15 @@ router.post("/logout", authenticationController.logout_post);
 router.get("/auth", verifyUser, authenticationController.authenticate_user);
 
 //Blog Post Routes
-router.post("/posts", verifyToken, postController.create_post);
-router.get("/posts", postController.posts_get);
-router.get("/posts/:postID", postController.post_details_get);
-router.delete("/posts/:postID", verifyToken, postController.post_delete);
-router.put("/posts/:postID", verifyToken, postController.post_edit_put);
+router.post("/posts", verifyUser, verifyToken, postController.create_post);
+router.get("/posts", verifyUser, postController.posts_get);
+router.get("/posts/:postID", verifyUser, postController.post_details_get);
+router.delete("/posts/:postID", verifyUser, verifyToken, postController.post_delete);
+router.put("/posts/:postID", verifyUser, verifyToken, postController.post_edit_put);
 
 //Comment Routes
-router.get("/posts/:postID/comments", commentController.comments_get);
-router.post("/posts/:postID/comments", verifyToken, commentController.comment_create_post);
-router.delete("/posts/:postID/comments/:commentID", verifyToken, commentController.comment_delete);
+router.get("/posts/:postID/comments", verifyUser, commentController.comments_get);
+router.post("/posts/:postID/comments", verifyUser, verifyToken, commentController.comment_create_post);
+router.delete("/posts/:postID/comments/:commentID", verifyUser, verifyToken, commentController.comment_delete);
 
 module.exports = router;
