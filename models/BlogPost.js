a0b4255e5d265a01fe3144583ec74346565cc2d2
basnetrajpradip <basnetrajpradip@gmail.com>
2024-03-23 23:05:41 +0545

    Create blog post schema

---
+++ models/BlogPost.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const TimeAgo = require("javascript-time-ago");
+const en = require("javascript-time-ago/locale/en");
+
+const BlogPostSchema = new Schema({
+  title: { type: String, required: true },
+  image: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comments", required: true }],
+  timeStamp: { type: Date, required: true, default: Date.now },
+});
+
+BlogPostSchema.virtual("formattedTimestamp").get(function () {
+  return TimeAgo.format(this.timestamp);
+});
+
+module.exports = mongoose.model("BlogPost", BlogPostSchema);
